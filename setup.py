0e2961e554c2ad38b41653423511667fb373371e
frankbu <frankb@ca.ibm.com>
2014-05-16 16:55:42 -0400

    corrected setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='ldo-serverlib',
+      version='1.0',
+      description='LDA Server Library',
+      author='Your Name',
+      author_email='example@example.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+      install_requires=[#'lda-clientlib',
+                        'webob==1.4',
+                        'pycrypto==2.6.1',
+                        'pymongo==2.7',
+                        'isodate==0.5.0',
+                        'rdflib==4.1.2',
+                        'werkzeug==0.9.4']
+     )
