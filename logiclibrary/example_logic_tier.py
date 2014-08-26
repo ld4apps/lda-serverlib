6f2c76a6eac805a5ccc406e4e8c739025d395a47
Martin Nally <nally@us.ibm.com>
2014-08-25 20:09:10 -0400

    fix non-member-properties for container_from_membership_resource_in_query_string

--- logiclibrary/example_logic_tier.py
+++ logiclibrary/example_logic_tier.py
@@ -449,11 +449,12 @@ class Domain_Logic(object):
         self.add_container(document, container_url, membership_resource, membership_predicate, member_is_object, None, None)
         return document 
         
-    def container_from_membership_resource_in_query_string(self, container_url, membership_predicate, member_is_object=False):
+    def container_from_membership_resource_in_query_string(self, membership_predicate, member_is_object=False):
         if self.query_string.endswith('?non-member-properties'):
             qs = self.query_string[:-22]
         else:
             qs = self.query_string
+        container_url = url_policy.construct_url(self.request_hostname, self.tenant, self.namespace, self.document_id, self.extra_path_segments, qs)
         membership_resource = self.absolute_url(urllib.unquote(qs))
         document = self.create_container(container_url, membership_resource, membership_predicate, member_is_object)
         status, document = self.complete_result_document(document)
