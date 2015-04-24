877a069890f532450a682aa5df1a24d9034617bb
frankbu <frankb@ca.ibm.com>
2015-04-24 15:57:47 -0400

    change to release structure

--- logiclibrary/storage.py
+++
@@ -1,8 +0,0 @@
-import os
-import importlib
-
-if 'OPERATION_PRIMITIVES' in os.environ:
-    import_name = os.environ['OPERATION_PRIMITIVES']
-else:
-    import_name = 'operation_primitives' #assume it has the standard name and is on the python path
-operation_primitives = importlib.import_module(import_name)
